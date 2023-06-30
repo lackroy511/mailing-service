e67667e4bcc39c5461d3017a6ff354c728693a20
RomanBogdanov <lackroy5111@gmail.com>
2023-06-30 11:20:58 +0700

    Реализовал создание, удаление и редактирование пользователя.

--- mail_sender/urls.py
+++ mail_sender/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from mail_sender.apps import MailSenderConfig
-from mail_sender.views import index
+
+from mail_sender.views import index, user_management, del_user, edit_user
 
 app_name = MailSenderConfig.name
 
 urlpatterns = [
     path('', index, name='index'),
+    
+    path('user_management/', user_management, name='user_management'),
+    path('del_user/<int:pk>/', del_user, name='del_user'),
+    path('edit_user/<int:pk>/', edit_user, name='edit_user')
+    
 ]
 
--- mail_sender/views.py
+++ mail_sender/views.py
@@ -1,6 +1,97 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from mail_sender.models import Client
+
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'mail_sender/index.html')
+    """
+    Главная страница. 
+    """
+
+    context = {
+        'is_active_main': 'active'
+    }
+    return render(request, 'mail_sender/index.html', context=context)
+
+
+def user_management(request):
+    """
+    Управление пользователями
+    """
+
+    users_list = Client.objects.all()
+
+    context = {
+        'users_list': users_list
+    }
+
+    if request.method == 'POST':
+        Client.objects.create(
+            email=request.POST.get('email'),
+            first_name=request.POST.get('first_name'),
+            last_name=request.POST.get('last_name'),
+            surname=request.POST.get('surname'),
+            comment=request.POST.get('comment')
+        )
+
+    return render(request, 'mail_sender/mailing_management/user_management.html', context=context)
+
+
+def del_user(request, pk):
+    """
+    Управление пользователями: Удалить пользователя
+    """
+
+    try:
+        Client.objects.get(pk=pk).delete()
+    except ObjectDoesNotExist:
+        return redirect('http://127.0.0.1:8000/user_management/')
+
+    users_list = Client.objects.all()
+
+    context = {
+        'users_list': users_list
+    }
+
+    if request.method == 'POST':
+        Client.objects.create(
+            email=request.POST.get('email'),
+            first_name=request.POST.get('first_name'),
+            last_name=request.POST.get('last_name'),
+            surname=request.POST.get('surname'),
+            comment=request.POST.get('comment')
+        )
+
+    return render(request, 'mail_sender/mailing_management/user_management.html', context=context)
+
+
+def edit_user(request, pk):
+    """
+    Управление пользователями: Редактировать пользователя.
+    """
+    user = Client.objects.get(pk=pk)
+    
+    context = {
+        'email': user.email,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'surname': user.surname,
+        'comment': user.comment,
+    }
+    
+    if request.method == 'POST':
+        user.email = request.POST.get('email')
+        user.first_name = request.POST.get('first_name')
+        user.last_name = request.POST.get('last_name')
+        user.surname = request.POST.get('surname')
+        user.comment =request.POST.get('comment')
+        
+        user.save()
+        
+        return redirect('http://127.0.0.1:8000/user_management/')
+    
+    return render(request, 'mail_sender/mailing_management/edit_user.html', context=context)
