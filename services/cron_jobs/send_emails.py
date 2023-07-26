76e1dd00ff6c57c0c149b17ae287eede15a9f1bb
RomanBogdanov <lackroy5111@gmail.com>
2023-07-26 16:18:57 +0700

    Переделал скрипт отправки сообщений

--- services/cron_jobs/send_emails.py
+++ services/cron_jobs/send_emails.py
@@ -1,10 +1,10 @@
 #!/home/debian/.cache/pypoetry/virtualenvs/coursework-6-7n6ESyJz-py3.11/bin/python
 # Путь до интерпретатора Python в виртуальном окружении.
-import smtplib
 import argparse
 import os
 import sys
 import django
+from django.core.mail import send_mail
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath('send_emails.py'))))
 sys.path.append(BASE_DIR)
@@ -12,8 +12,8 @@ sys.path.append(BASE_DIR)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
-from config.settings import EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_USER, \
-    EMAIL_HOST_PASSWORD
+from config.settings import EMAIL_HOST_USER
+from mailing_management.models import Mailing
 
 
 def parse_args() -> argparse.Namespace:
@@ -33,9 +33,10 @@ def parse_args() -> argparse.Namespace:
     return args
 
 
-def send_emails(subject: str = 'ТЕСТОВОЕ СООБЩЕНИЕ',
-                massage: str = 'Оно приходит, когда что-то не работает',
-                email_list: str = 'lackroy511@gmail.com') -> None:
+def send_emails(subject: str,
+                massage: str,
+                sender: str,
+                email_list: str) -> None:
     """ Отправляет письма по списку адресов электронной почты.
     Args:
         subject (str): Тема письма
@@ -43,49 +44,23 @@ def send_emails(subject: str = 'ТЕСТОВОЕ СООБЩЕНИЕ',
         email_list (str): Список адресов электронной почты, одной строкой,
                           разделенных символом пробела.
     """
-
-    server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
-    server.connect(EMAIL_HOST, EMAIL_PORT)
-    server.ehlo()
-    server.starttls()
-    server.ehlo()
-
-    login = EMAIL_HOST_USER
-    password = EMAIL_HOST_PASSWORD
-    sender = EMAIL_HOST_USER
-    server.login(login, password)
-
-    # Формирование сообщения
-    email = f'Subject: {subject}\n\n{massage}'
-
-    # Русский не поддерживается, поэтому энкодим
-    email = email.encode("UTF-8")
-
-    # Разделение списка адресов по пробелам, если адресов в строке больше двух
-    if email_list.count(' ') > 0:
-        email_list = email_list.split(' ')
-
-    if isinstance(email_list, list):
-        for receiver in email_list:
-            server.sendmail(sender, receiver, email)
-    else:
-        server.sendmail(sender, email_list, email)
-
-    server.quit()
+    email_list = email_list.split(' ')
+    send_mail(subject, massage, sender, email_list)
 
 
 def main() -> None:
     args = parse_args()
 
-    from mailing_management.models import Mailing
-
     mailing = Mailing.objects.get(pk=args.pk)
 
     mailing.mailingsettings.mailing_status = 'отправляется'
+    mailing.mailingsettings.save()
     send_emails(subject=args.subject,
                 massage=args.massage,
+                sender=EMAIL_HOST_USER,
                 email_list=args.email_list)
     mailing.mailingsettings.mailing_status = 'завершена'
+    mailing.mailingsettings.save()
 
 
 if __name__ == '__main__':
